46a6157cc784f2c89b142a23abc57dfc5a83f932
Aaron Cummins <acummins@leappoint.com>
2026-03-19 08:58:36 -0400

    Add new profile pages and enhance migration script for authorized admins and specializations
    - Introduced a new Excel file for profile pages.
    - Updated the Groovy migration script to:
      - Clean up authorized admins string by removing parentheses.
      - Migrate areas of specialization as a pipe-separated list.
      - Create necessary subfolders in the target directory based on workday IDs.
      - Ensure proper handling of profiles not in the workday report.

--- ProfileCF/detectAndCreateCF.py
+++ ProfileCF/detectAndCreateCF.py
@@ -62,6 +62,7 @@ def expand_elements():
     # Output terminal text to log file as well
     log_file = open("detectAndCreateCF_log.txt", "w")
     failed_log_file = open("detectAndCreateCF_failed_log.txt", "w")
+    success_page_list = open("detectAndCreateCF_success_pages.txt", "w")
 
     # --- Find header columns ---
     header_row = 1
@@ -86,26 +87,29 @@ def expand_elements():
     sadeaWb = pd.read_excel("sadeaList.xlsx", sheet_name="Sheet1")
     sadeaMap = {}
     for index, row in sadeaWb.iterrows():
-        sadeaMap[row['Eaglenet ID']] = row
+        sadeaMap[row['eaglenet_id']] = row
+
+    # -- load new profile page list and create map of eaglenet ids to new profile pages --
+    newProfilePagesWb = pd.read_excel("new_profile_pages.xlsx", sheet_name="Sheet1")
+    newProfilePagesMap = {}
+    for index, row in newProfilePagesWb.iterrows():
+        newProfilePagesMap[row['Eaglenet ID']] = row
     
     # --- Process URLs ---
     cfs = []
     for row_idx_place, id in enumerate(idsToProcess):
 
         #stop after 10 for testing
-       # if row_idx_place > 10:
+        #if row_idx_place > 10:
         #    break
 
-        if id not in eaglenetIdMap:
-            print(f"⚠️ Eaglenet ID {id} not found in report")
-            log_file.write(f"! Eaglenet ID {id} not found in report\n")
-            continue
+        inIdMap = id in eaglenetIdMap
 
         url_val = ''
-        defaultProfilePage = eaglenetIdMap[id]['Default Profile Page']
+        defaultProfilePage = eaglenetIdMap[id]['Default Profile Page'] if inIdMap else None
         if defaultProfilePage is None or not isinstance(defaultProfilePage, str) or defaultProfilePage.strip() == '':
 
-            allProfilePages = eaglenetIdMap[id]['All Profile Pages']
+            allProfilePages = eaglenetIdMap[id]['All Profile Pages'] if inIdMap else None
             if allProfilePages is not None and isinstance(allProfilePages, str) and allProfilePages.strip() != '':
                 additional_urls_val = allProfilePages.strip().lower()
                 additional_urls_val = additional_urls_val.replace('faculty:', '')
@@ -113,17 +117,22 @@ def expand_elements():
                 additional_urls_val = additional_urls_val.replace('student:', '')
                 for additional_url_val in additional_urls_val.split('|'):
                     if additional_url_val != '':
-                        url_val = additional_url_val
+                        url_val = additional_url_val.strip().lower()
                         break
         else:
             url_val = defaultProfilePage.strip().lower()
             
-
         if url_val == '':
             if id in sadeaMap:
                 sadeaProfilePage = sadeaMap[id]['full url']
                 if sadeaProfilePage is not None and isinstance(sadeaProfilePage, str) and sadeaProfilePage.strip() != '':
                     url_val = sadeaProfilePage.strip()
+
+        if url_val == '':
+            if id in newProfilePagesMap:
+                newProfilePage = newProfilePagesMap[id]['URL']
+                if newProfilePage is not None and isinstance(newProfilePage, str) and newProfilePage.strip() != '':
+                    url_val = newProfilePage.strip()
 
         if url_val == '':
             print(f"❌ No URL found for Eaglenet ID {id}")
@@ -148,12 +157,14 @@ def expand_elements():
                 if len(profilesElement) == 0:
                     print(f"⚠️ {url_val} → No '{ELEMENT_SELECTOR}' elements found")
                     log_file.write(f"! {url_val} -> No '{ELEMENT_SELECTOR}' elements found\n")
+                    failed_log_file.write(f"! {url_val} -> No '{ELEMENT_SELECTOR}' elements found\n")
                     continue
 
                 # If more than one element found, print error, continue to next URL
                 if len(profilesElement) != 1:
                     print(f"⚠️ {url_val} → Expected 1 '{ELEMENT_SELECTOR}' element, found {len(profilesElement)}")
                     log_file.write(f"! {url_val} -> Expected 1 '{ELEMENT_SELECTOR}' element, found {len(profilesElement)}\n")
+                    failed_log_file.write(f"! {url_val} -> Expected 1 '{ELEMENT_SELECTOR}' element, found {len(profilesElement)}\n")
                     continue
 
                 profilesElement = profilesElement[0]
@@ -166,8 +177,9 @@ def expand_elements():
                 if profileContentSection is None:
                     print(f"⚠️ {url_val} → No 'section.profile-content' found")
                     log_file.write(f"! {url_val} -> No 'section.profile-content' found\n")
+                    failed_log_file.write(f"! {url_val} -> No 'section.profile-content' found\n")
                     continue
-                forceDisplay = eaglenetIdMap[id]['Force Profile']
+                forceDisplay = eaglenetIdMap[id]['Force Profile'] if inIdMap else ''
 
                 # Get Bio html
                 bioElement = profilesElement.css.select_one("dd.bio-text")
@@ -208,8 +220,28 @@ def expand_elements():
                         header.decompose()
                 scholarlyHtml = scholarlyElement.decode_contents() if scholarlyElement else ''
 
+                """ # Get Scholarly html
+                scholarlyElement = profilesElement.css.select_one("section#profile-activities > div")
+                # select Scholarly entries which are in divs with class col-md-6
+                scholarlyEntries = scholarlyElement.css.select("div.col-md-6 > div") if scholarlyElement else []
+                scholarlyHtml = ''
+                # loop through scholarly entries, append title (h3) and content (div) to scholarlyHtml
+                # seperate entries with $#! to allow splitting in CF
+                # seperate title and content with !#$ to allow splitting in CF
+                for entry in scholarlyEntries:
+                    titleElement = entry.css.select_one("h3")
+                    titleText = titleElement.text.strip() if titleElement else ''
+                    # remove title element from entry to avoid duplication in content
+                    if titleElement:
+                        titleElement.decompose()
+                    contentHtml = entry.decode_contents()
+                    scholarlyHtml += titleText + '!#$' + contentHtml + '$#!'
+                # remove trailing $#! from scholarlyHtml
+                if scholarlyHtml.endswith('$#!'):
+                    scholarlyHtml = scholarlyHtml[:-3] """
+
                 # Get contact info element
-                contactInfoElement = profilesElement.css.select_one("dl#profile-contact-info")
+                contactInfoElement = profilesElement.css.select_one("dl.profile-contact-info")
 
                 # Get office hours html from last dd in contact info
                 officeHoursHtml = ''
@@ -223,37 +255,66 @@ def expand_elements():
                 altPhoneType = ''
                 if contactInfoElement:
                     altPhoneNumberElement = contactInfoElement.css.select_one("dd.profile-phone > a")
-                    if (altPhoneNumberElement.get('itemprop', '') == 'faxNumber'):
+                    if (altPhoneNumberElement and altPhoneNumberElement.get('itemprop', '') == 'faxNumber'):
                         altPhoneNumber = altPhoneNumberElement.text.strip() if altPhoneNumberElement else ''
                         altPhoneNumberElement.decompose()  # remove phone number element to avoid duplication
                         altPhoneTypeElement = contactInfoElement.css.select_one("dd.profile-phone")
                         altPhoneType = altPhoneTypeElement.text.strip() if altPhoneTypeElement else ''
+
+                # Get hide email value from contact info
+                hideEmail = 'false'
+                if contactInfoElement:
+                    emailElement = contactInfoElement.css.select_one("dd.profile-email")
+                    if emailElement and emailElement.text.strip() != '':
+                        hideEmail = 'false'
+                    else:
+                        hideEmail = 'true'
+
+                # Get hide phone value from contact info
+                hidePhone = 'false'
+                if contactInfoElement:
+                    phoneElement = contactInfoElement.css.select_one("dd.profile-phone")
+                    if phoneElement and phoneElement.text.strip() != '':
+                        hidePhone = 'false'
+                    else:
+                        hidePhone = 'true'
                 
                 # Get See also links
                 contactLinksHtml = ''
                 seeAlsoLinks = profilesElement.css.select("div.profile-see-also > dl > dd")
                 for link in seeAlsoLinks:
-                    # if previous sibling dt text is 'For the Media', skip this link
+                    # if previous sibling dt text is 'See Also', add this link to contactLinksHtml
                     prev_dt = link.find_previous_sibling('dt')
-                    if prev_dt and prev_dt.text.strip().lower() == 'for the media':
-                        continue
-                    contactLinksHtml += link.decode_contents() + '<br>'
+                    if prev_dt and prev_dt.text.strip().lower() == 'see also':
+                        contactLinksHtml += link.decode_contents() + '<br>'
+
+                # Get areas of specialization
+                areasOfSpecializationMigrated = ''
+                areasOfSpecialization = profilesElement.css.select("div.profile-see-also > dl > dd")
+                for entry in areasOfSpecialization:
+                    # if previous sibling dt text is 'Areas of Specialization', add this entry to areasOfSpecializationHtml
+                    prev_dt = entry.find_previous_sibling('dt')
+                    if prev_dt and prev_dt.text.strip().lower() == 'areas of specialization':
+                        areasOfSpecializationMigrated += entry.text + '|'
+                # remove trailing | from areasOfSpecializationMigrated
+                if areasOfSpecializationMigrated.endswith('|'):
+                    areasOfSpecializationMigrated = areasOfSpecializationMigrated[:-1]
 
                 # Add resume if present
-                resume = eaglenetIdMap[id]['Resume']
+                resume = eaglenetIdMap[id]['Resume'] if inIdMap else ''
                 if resume and isinstance(resume, str) and resume.strip() != '':
                     resume = resume.strip()
                     resume = BASE_ASSET_PATH + '/migrated-profile-resumes/' + resume.lstrip('/')
 
                 # Overwrite resume with CV if present
-                cv = eaglenetIdMap[id]['CV']
+                cv = eaglenetIdMap[id]['CV'] if inIdMap else ''
                 if cv and isinstance(cv, str) and cv.strip() != '':
                     cv = cv.strip()
                     cv = BASE_ASSET_PATH + '/migrated-profile-resumes/' + cv.lstrip('/')
                     resume = cv  # overwrite resume with CV
 
                 # Add profile image if present and not default
-                profileImage = eaglenetIdMap[id]['Profile Image']
+                profileImage = eaglenetIdMap[id]['Profile Image'] if inIdMap else ''
                 if profileImage and isinstance(profileImage, str) and profileImage.strip() != '':
                     profileImage = profileImage.strip()
                     if not profileImage.lower().endswith('/uploads/defaults/original/au_profile.jpg'):
@@ -262,7 +323,7 @@ def expand_elements():
                         profileImage = '/content/dam/au/assets/global/images/au_profile.jpg'  # default image
 
                 # Add authorized admins if present
-                authorizedAdmins = eaglenetIdMap[id]['Authorized Admins']
+                authorizedAdmins = eaglenetIdMap[id]['Authorized Admins'] if inIdMap else ''
                 authorizedAdminsString = ''
                 if authorizedAdmins and isinstance(authorizedAdmins, str) and authorizedAdmins.strip() != '':
                     for admin in authorizedAdmins.split(','):
@@ -297,22 +358,26 @@ def expand_elements():
                     "photo": profileImage,
                     "defaultProfilePage": url_val.replace('https://www.american.edu', '').replace('.cfm', ''),
                     "authorizedAdminsMigrated": authorizedAdminsString,
+                    "username": id,
+                    "hideEmail": hideEmail,
+                    "hidePhone": hidePhone,
+                    "areasOfSpecializationMigrated": areasOfSpecializationMigrated
                 })
 
+                print(f"✅ Processed #{row_idx_place}/{len(idsToProcess)}: {url_val}")
+                log_file.write(f"O Processed #{row_idx_place}/{len(idsToProcess)}: {url_val}\n")
+                success_page_list.write(f"{url_val}\n")
             else:
                 print(f"⚠️ {url_val} → HTTP {response.status_code}")
                 log_file.write(f"! {url_val} -> HTTP {response.status_code}\n")
+                failed_log_file.write(f"! {url_val} -> HTTP {response.status_code}\n")
         except requests.exceptions.RequestException:
             print(f"❌ Failed to fetch {url_val}")
             log_file.write(f"X Failed to fetch {url_val}\n")
-            print("----------------------------------")
-            log_file.write("----------------------------------\n")
             failed_log_file.write(f"{url_val}\n")
             continue
 
 
-        print(f"✅ Processed #{row_idx_place}/{len(idsToProcess)}: {url_val}")
-        log_file.write(f"O Processed #{row_idx_place}/{len(idsToProcess)}: {url_val}\n")
         print("----------------------------------")
         log_file.write("----------------------------------\n")
 
--- groovyscripts/migrated-profiles.groovy
+++ groovyscripts/migrated-profiles.groovy
@@ -59,6 +59,9 @@ for (def prefixFolder : profilesFolderChildren) {
 
         if (cfNode.hasProperty("jcr:content/data/master/authorizedAdminsMigrated")) {
             def authorizedAdminsMigrated = cfNode.getProperty('jcr:content/data/master/authorizedAdminsMigrated').getString()
+            // Remove "( and )" from the string if they exist
+            authorizedAdminsMigrated = authorizedAdminsMigrated.replaceAll("\"(", "\"")
+            authorizedAdminsMigrated = authorizedAdminsMigrated.replaceAll(")\"", "\"")
             
             // Assuming authorizedAdminsMigrated is a pipe-separated list of admin names
             def adminNames = authorizedAdminsMigrated.split("\\|")
@@ -69,6 +72,19 @@ for (def prefixFolder : profilesFolderChildren) {
             propertiesNode.setProperty("authorizedAdminsCMF", adminNames)
             resourceResolver.commit()
         }
+
+        if (cfNode.hasProperty("jcr:content/data/master/areasOfSpecializationMigrated")) {
+            def areasOfSpecializationMigrated = cfNode.getProperty('jcr:content/data/master/areasOfSpecializationMigrated').getString()
+            
+            // Assuming areasOfSpecializationMigrated is a pipe-separated list of specialization names
+            def specializationNames = areasOfSpecializationMigrated.split("\\|")
+            def propertiesNode = getNode("${cfNode.path}/jcr:content/data/master")
+            println("Migrated specializations for ${propertiesNode.path}: ${specializationNames}")
+
+            // Write String[] prop to node
+            propertiesNode.setProperty("areasOfSpecialization", specializationNames)
+            resourceResolver.commit()
+        }
         
         def existingCF = getResource(newPath.toString())
         if (existingCF != null) {
@@ -77,6 +93,81 @@ for (def prefixFolder : profilesFolderChildren) {
         }
         session.workspace.copy(cfResource.path, newPath.toString())
         println(newPath.toString())
+        
+    }
+}
+
+def getSubFolderResource(parentFolderResource, subFolderName) throws PersistenceException {
+    subFolderName = subFolderName.replaceAll("\\W", "_");
+    subFolderResource = parentFolderResource.getChild(subFolderName);
+    if (subFolderResource == null) {
+        def nodeParams = new HashMap<>();
+        nodeParams.put("jcr:primaryType", "sling:Folder");
+        subFolderResource = resourceResolver.create(parentFolderResource, subFolderName, nodeParams);
+        resourceResolver.commit();
+    }
+    return subFolderResource;
+}
+
+def profilesFolder = getResource("/content/dam/au/cf/profiles-migrated")
+def targetProfilesFolder = getResource("/content/dam/au/cf")
+targetProfilesFolder = getSubFolderResource(targetProfilesFolder, "profiles")
+def profilesFolderChildren = profilesFolder.getChildren()
+
+println("${profilesFolderChildren.size()}")
+
+def count = 0
+
+for (def prefixFolder : profilesFolderChildren) {
+    
+    for (def cfFolder : prefixFolder.getChildren()) {
+        if(cfFolder.name == "jcr:content") {
+            continue
+        }
+        
+        def cfResource = cfFolder.getChild("profileCF")
+        if (cfResource == null) {
+            println('oopsie')
+            continue
+        }
+        def cfNode = getNode(cfResource.path)
+        
+        def newPath = cfResource.path.replace("profiles-migrated", "profiles/not_in_workday_report")
+        def notInWorkdayReportFolder = getSubFolderResource(targetProfilesFolder, "not_in_workday_report")
+        if (cfNode.hasProperty("jcr:content/data/master/workdayID")) {
+            def workdayId = cfNode.getProperty('jcr:content/data/master/workdayID').getString()
+            def subFolderName = "00";
+            if (workdayId.length() >= 6) {
+                subFolderName = workdayId.substring(workdayId.length() - 6, workdayId.length() - 4);
+            }
+            newPath = "/content/dam/au/cf/profiles/${subFolderName}/${workdayId}/${workdayId}"
+            def newPrefixFolder = getSubFolderResource(targetProfilesFolder, subFolderName)
+            def newCfFolder = getSubFolderResource(newPrefixFolder, workdayId)
+            //println(newPath)
+
+            
+        } else {
+            // create last two folders if they don't exist
+            println(newPath.split("/")[7])
+            println(newPath.split("/")[8])
+            def newPrefixFolder = getSubFolderResource(notInWorkdayReportFolder, newPath.split("/")[7])
+            def newCfFolder = getSubFolderResource(newPrefixFolder, newPath.split("/")[8])
+        }
+
+        if (cfNode.hasProperty("jcr:content/data/master/authorizedAdminsMigrated")) {
+            def authorizedAdminsMigrated = cfNode.getProperty('jcr:content/data/master/authorizedAdminsMigrated').getString()
+            
+            // Assuming authorizedAdminsMigrated is a pipe-separated list of admin names
+            def adminNames = authorizedAdminsMigrated.split("\\|")
+            //println("Migrated admins for ${cfResource.name}: ${adminNames}")
+
+            // Write String[] prop to node
+            def targetNode = getNode("${cfNode.path}/jcr:content/data/master")
+            println(targetNode.path)
+            targetNode.setProperty("authorizedAdmins", adminNames)
+        }
+        
+        session.workspace.copy(cfReszurce.path, newPath.toString())
         
     }
 }
